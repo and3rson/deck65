84aa77860729657899f3283852998c5b66376670
Andrew Dunai <a@dun.ai>
2023-03-08 18:21:46 +0200

    Improve LCD code, add basic SID support

--- rom/src/init.s
+++ rom/src/init.s
@@ -1,37 +1,125 @@
+.segment "ZEROPAGE"
+
+PTR: .res 2
+
 .segment "CODE"
 
+S_BAR19: .byte "===================\n\x00"
 S_HELLO: .byte "Hello there!\n\x00"
 S_SYSTEM: .byte "64K RAM SYSTEM\n\x00"
-S_READY: .byte "READY\n\x00"
+S_READY: .byte "READE\x08YX\x08\n\x00"
+S_LOADING: .byte "Loading song...\n\x00"
+S_INITIALIZING: .byte "Initializing...\n\x00"
+S_STARTING: .byte "Starting...\n\x00"
+S_PROGRESS: .byte ".\n\x00"
 
 
 ; Kernel entrypoint
 ; Arguments: none
 init:
-    jsr lcd_init
-    jsr lcd_clear
+        lda #%10101010 ; digital analyzer trigger
 
-    lda #<S_HELLO
-    ldx #>S_HELLO
-    jsr lcd_printz
+        jsr lcd_init
 
-    jsr busywait
+        ; ldx #8
+        ; ldy #2
+        ; jsr lcd_gotoxy
 
-    lda #<S_SYSTEM
-    ldx #>S_SYSTEM
-    jsr lcd_printz
+        lda #<S_BAR19
+        ldx #>S_BAR19
+        jsr lcd_printz
 
-    jsr busywait
+        lda #<S_HELLO
+        ldx #>S_HELLO
+        jsr lcd_printz
 
-    lda #<S_READY
-    ldx #>S_READY
-    jsr lcd_printz
+        ; jsr busywait
 
-; TODO: Load this at 0FFF
+        lda #<S_SYSTEM
+        ldx #>S_SYSTEM
+        jsr lcd_printz
+
+        ; ; ; jsr busywait
+
+        lda #<S_READY
+        ldx #>S_READY
+        jsr lcd_printz
+
+        ; lda #<S_LOADING
+        ; ldx #>S_LOADING
+        ; jsr lcd_printz
+
+    ; @tmp:
+    ;     lda #15
+    ;     sta $D418
+    ;     lda #<S_PROGRESS
+    ;     ldx #>S_PROGRESS
+    ;     jsr lcd_printz
+    ;     lda #0
+    ;     sta $D418
+    ;     lda #<S_PROGRESS
+    ;     ldx #>S_PROGRESS
+    ;     jsr lcd_printz
+    ;     jmp @tmp
+
+        ; load song into RAM
+        ; ldy #0
+    ; @copy_byte:
+        ; lda songdata,Y
+        ; sta songdest,Y
+        ; lda PTR
+        ; inc
+        ; sta PTR
+        ; iny
+        ; cpy #songlen
+        ; bne @copy_byte
+
+        ; lda #<S_INITIALIZING
+        ; ldx #>S_INITIALIZING
+        ; jsr lcd_printz
+
+        ; ; init song
+        ; jsr songinit
+
+        ; lda #<S_STARTING
+        ; ldx #>S_STARTING
+        ; jsr lcd_printz
+
+    ; @play:
+        ; ; play song
+        ; jsr songplay
+
+        ; lda #<S_PROGRESS
+        ; ldx #>S_PROGRESS
+        ; jsr lcd_printz
+
+        ; jmp @play
+
+    songstart:
 ; .incbin "./music/mca_vrolijke_vier.sid"
+; this currently works only for songs <256 bytes
+.incbin "./music/Crue_Gurl_Freestyle_Remix.sid"
+; .incbin "./music/Room_2.sid"
+    songend:
+
+    songdata = songstart + $7E
+    songlen = songend - songdata
+    ; songdest = $0FFF
+    songdest = $1000
+    songinit = $1006
+    songplay = $1000
+    ; songdest = $10EB
+    ; songinit = $10EB
+    ; songplay = $1102
+
+    .byte $AA, $BB
+    foo1: .byte >songstart
+    foo2: .byte <songstart
+    foo3: .byte >songdata
+    foo4: .byte <songdata
 
 ; TODO: Basic basic
 ; TODO: LOAD from SD card
 
-    lda LCD0 ; for debug
-    stp
+        lda LCD0 ; for debug
+        stp
--- rom/src/lcd.s
+++ rom/src/lcd.s
@@ -1,3 +1,5 @@
+; LCD implementation with constant tty-like scrolling
+
 .segment "ZEROPAGE"
 
 ; generic 16-bit pointer for string operations
@@ -5,9 +7,16 @@ LCD_PTR: .res 2
 
 LCD_CURSOR_X: .res 1
 LCD_CURSOR_Y: .res 1
-LCD_CHARS: .res 80
+LCD_BUFFER: .res 80
+LCD_BUFFER_20 = LCD_BUFFER + 20
+LCD_BUFFER_40 = LCD_BUFFER + 40
+LCD_BUFFER_60 = LCD_BUFFER + 60
+
+P_DD_LINE_ADDR: .res 2
 
 .segment "CODE"
+
+DD_LINE_ADDR: .byte 0, 64, 20, 84
 
 ; Wait until LCD is ready
 ; Arguments: none
@@ -23,20 +32,58 @@ lcd_busy:
         rts
 
 
+; Move LCD cursor
+; Arguments:
+;   X - column
+;   Y - row
+lcd_gotoxy:
+        pha
+        phx
+        phy
+
+        stx LCD_CURSOR_X
+        sty LCD_CURSOR_Y
+        ; Get DDRAM addr for line start
+        lda (P_DD_LINE_ADDR), Y
+        ; Add X
+        clc
+        adc LCD_CURSOR_X
+        ; Add instruction flag
+        ora #$80
+        ; Move cursor
+        sta LCD0
+        jsr lcd_busy
+
+    @end:
+        ply
+        plx
+        pla
+
+        rts
+
 ; Initialize LCD
 ; Arguments: none
 lcd_init:
         pha
         phx
+        phy
 
-        lda #0
-        sta LCD_CURSOR_X
-        sta LCD_CURSOR_Y
+        ; Set up pointer to array with line addresses
+        lda #<DD_LINE_ADDR
+        sta P_DD_LINE_ADDR
+        lda #>DD_LINE_ADDR
+        sta P_DD_LINE_ADDR+1
+
+        ; Clear screen buffer
+    @clear:
+        lda #' '
+        sta LCD_BUFFER, X
+        inx
+        cpx #80
+        bne @clear
 
         ldx #$04
     @repeat:
-        lda #%10101010 ; digital analyzer trigger
-
         lda #%00111000 ; 8 bit, 2 lines, 5x8
         sta LCD0
         jsr lcd_busy
@@ -52,11 +99,16 @@ lcd_init:
         sta LCD0
         jsr lcd_busy
 
-        lda #%10000000 ; ddgram address set: $00
-        sta LCD0
-        jsr lcd_busy
+        ; lda #%10000000 ; ddgram address set: $00
+        ; lda #(DD_LINE_ADDR | %10000000) ; ddram address set: line 3 start
+        ; sta LCD0
+        ; jsr lcd_busy
+
+        ; Clear screen
+        jsr lcd_clear
 
     @end:
+        ply
         plx
         pla
 
@@ -67,14 +119,20 @@ lcd_init:
 ; Arguments: none
 lcd_clear:
         pha
+        phx
+        phy
 
         lda #%00000001 ; clear
         sta LCD0
         jsr lcd_busy
-        lda #%10000000 ; ddram address set: $00
-        sta LCD0
-        jsr lcd_busy
 
+        ; Set cursor pos
+        ldx #0
+        ldy #3
+        jsr lcd_gotoxy
+
+        ply
+        plx
         pla
 
         rts
@@ -86,86 +144,146 @@ lcd_clear:
 lcd_printchar:
         pha
         phx
+        phy
 
+        ; Check if \n
         cmp #10
-        bne @normal
-        ; Fill with spaces
-        ldx LCD_CURSOR_X
-    @whitespace:
+        beq @newline
+
+        cmp #8
+        beq @backspace
+
+        ; Check if has space
+        tax
+        lda LCD_CURSOR_X
+        cmp #19
+        beq @end  ; no more space
+
+        ; Print character
+        stx LCD1
+        jsr lcd_busy
+
+        ; Char X-pos -> X, char code -> A
+        phx
+        tax
+        pla
+
+        ; Write to screen buffer
+        sta LCD_BUFFER_60, X
+
+        ; Increase cursor X-pos
+        inx
+        stx LCD_CURSOR_X
+
+        jmp @end
+
+    @newline:
+        ; Scroll screen memory up by 20 bytes
+        ldx #0
+    @scroll1:
+        lda LCD_BUFFER_20, X
+        sta LCD_BUFFER, X
+        inx
+        cpx #60
+        bne @scroll1
+
+        ; Fill line 3 with spaces
+        ldx #0
+    @add_space:
+        lda #' '
+        sta LCD_BUFFER_60, X
+        inx
+        cpx #20
+        bne @add_space
+
+        jsr lcd_redraw
+
+        ldx #0
+        ldy #3
+        jsr lcd_gotoxy
+
+        jmp @end
+
+    @backspace:
+        lda LCD_CURSOR_X
+        beq @end  ; already at first column
+        ; Update cursor position & screen buffer
+        tax
+        dex
+        stx LCD_CURSOR_X
+        lda #' '
+        sta LCD_BUFFER_60, X
+        ; Move cursor left
+        lda #%00010000
+        sta LCD0
+        jsr lcd_busy
+        ; Write space
         lda #' '
         sta LCD1
         jsr lcd_busy
-        inx
-        txa
-        cmp #20
-        bne @whitespace
-
-        jmp @cr
-    @normal:
-        ; Print character
-        sta LCD1
+        ; Move cursor left
+        lda #%00010000
+        sta LCD0
         jsr lcd_busy
 
-        ; Check cursor X
-        ldx LCD_CURSOR_X
-        inx
-        stx LCD_CURSOR_X
-        txa
-        ; If not at EOL, end
-        cmp #20
-        bne @end
-
-    @cr:
-        ; Carriage return
-        ldx #0
-        stx LCD_CURSOR_X
-
-        ; Line feed
-        ldx LCD_CURSOR_Y
-        inx
-        txa
-        cmp #4
-        bne @savecursor
-
-        ; Wrap cursor to top line
-        ldx #0
-    @savecursor:
-        stx LCD_CURSOR_Y
-
-        ; Update cursor position
-        txa
-        cmp #1
-        beq @gotoline1
-        cmp #2
-        beq @gotoline2
-        cmp #3
-        beq @gotoline3
-
-    @gotoline0:
-        ; Y=0: ADD=0
-        lda #0
-        jmp @writeline
-
-    @gotoline1:
-        ; Y=1: 64
-        lda #64
-        jmp @writeline
-
-    @gotoline2:
-        ; Y=2: 20
-        lda #20
-        jmp @writeline
-
-    @gotoline3:
-        ; Y=3: 84
-        lda #84
-
-    @writeline:
-        ; Write new position to DDRAM
-        ora #%10000000
-        sta LCD0
-
     @end:
+        ply
+        plx
+        pla
+
+        rts
+
+; Redraw entire screen from memory buffer
+lcd_redraw:
+        pha
+        phx
+        phy
+
+        ldx #0
+        ldy #0
+        jsr lcd_gotoxy
+    @print_line0:
+        lda LCD_BUFFER, X
+        sta LCD1
+        jsr lcd_busy
+        inx
+        cpx #20
+        bne @print_line0
+
+        ldx #0
+        ldy #1
+        jsr lcd_gotoxy
+    @print_line1:
+        lda LCD_BUFFER_20, X
+        sta LCD1
+        jsr lcd_busy
+        inx
+        cpx #20
+        bne @print_line1
+
+        ldx #0
+        ldy #2
+        jsr lcd_gotoxy
+    @print_line2:
+        lda LCD_BUFFER_40, X
+        sta LCD1
+        jsr lcd_busy
+        inx
+        cpx #19
+        bne @print_line2
+
+        ldx #0
+        ldy #3
+        jsr lcd_gotoxy
+    @print_line3:
+        lda LCD_BUFFER_60, X
+        sta LCD1
+        jsr lcd_busy
+        inx
+        cpx #19
+        bne @print_line3
+
+        ply
         plx
         pla
 
@@ -185,7 +303,7 @@ lcd_printz:
         sta LCD_PTR
         stx LCD_PTR+1
 
-        ldx #0
+        ldy #0
 
     @printchar:
         lda (LCD_PTR), Y
