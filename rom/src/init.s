151de8bdbef34405285f2ced80debb64f1d74863
Andrew Dunai <a@dun.ai>
2023-03-11 21:23:37 +0200

    Cleanup init.s

--- rom/src/init.s
+++ rom/src/init.s
@@ -18,152 +18,35 @@ S_PROGRESS: .asciiz ".\n"
 ; Arguments: none
 init:
         sei
-        jsr kbd_init
         jsr interrupts_init
+        jsr kbd_init
+        jsr lcd_init
         cli
 
-        ; cli
-
-        ; stp
-
-        ; lda #%10101010 ; digital analyzer trigger
-
-        ; ; Bit-bang all bits of VIA (port B)
-        ; lda #$FF
-        ; sta VIA1_DDRB
-        ; sta VIA1_RB
-    ; @flip:
-        ; inc VIA1_RB
-        ; dec VIA1_RB
-        ; inx
-        ; dex
-        ; jmp @flip
-
-        jsr lcd_init
-
-        ; ldx #8
-        ; ldy #2
-        ; jsr lcd_gotoxy
-
-        ; lda #<S_BAR19
-        ; ldx #>S_BAR19
-        ; jsr lcd_printz
+        lda #<S_BAR19
+        ldx #>S_BAR19
+        jsr lcd_printz
 
         lda #<S_HELLO
         ldx #>S_HELLO
         jsr lcd_printz
 
-        ; lda LCD_INIT
-        ; jsr lcd_printhex
+        lda #<S_SYSTEM
+        ldx #>S_SYSTEM
+        jsr lcd_printz
 
-    ; @getch:
-    ;     lda KBD_RDY
-    ;     beq @getch  ; If zero - restart
-    ;     lda KBD_REG
-    ;     jsr lcd_printhex  ; Otherwise - print to screen
-    ;     lda #0
-    ;     sta KBD_RDY  ; Clear keyboard ready flag
-    ;     ; lda #10
-    ;     ; jsr lcd_printchar
-    ;     jmp @getch
+        lda #<S_READY
+        ldx #>S_READY
+        jsr lcd_printz
 
     ; REPL loop
     @loop:
-        ; lda #<S_HELLO
-        ; ldx #>S_HELLO
-        ; jsr lcd_printz
-        ; lda #$FF
-        ; ldx #$FF
-        ; jsr vdelay
-        ; lda #<S_BAR19
-        ; ldx #>S_BAR19
-        ; jsr lcd_printz
-        ; lda #$FF
-        ; ldx #$FF
-        ; jsr vdelay
         jsr kbd_getch
         jsr lcd_printchar
-        ; jsr lcd_printhex
-        ; lda #','
-        ; jsr lcd_printchar
+        cmp #10  ; Return pressed
+        bne @loop
+        ; TODO: Execute command
         jmp @loop
-
-        ; ; ; ; jsr busywait
-
-        ; lda #<S_SYSTEM
-        ; ldx #>S_SYSTEM
-        ; jsr lcd_printz
-
-        ; ; ; ; ; ; jsr busywait
-
-        ; lda #<S_READY
-        ; ldx #>S_READY
-        ; jsr lcd_printz
-
-        ; ; print VIA values
-        ; ; lda VIA1_SR
-        ; ; jsr lcd_printhex
-        ; ; lda VIA1_ACR
-        ; ; jsr lcd_printhex
-        ; ; lda VIA1_PCR
-        ; ; jsr lcd_printhex
-        ; ; lda VIA1_IFR
-        ; ; jsr lcd_printhex
-        ; ; lda VIA1_IER
-        ; ; jsr lcd_printhex
-        ; ; lda #' '
-        ; ; jsr lcd_printchar
-
-        ; ; http://archive.6502.org/datasheets/wdc_w65c22_sep_13_2010.pdf, page 27
-        ; lda #%11000000  ; set timer1 interrupt enable flag
-        ; sta VIA1_IER
-
-        ; lda #%01000000  ; set timer1 to continuous interrupts, no PB7 toggle
-        ; sta VIA1_ACR
-
-        ; lda #$FF
-        ; sta VIA1_T1CL
-        ; lda #$40
-        ; sta VIA1_T1CH
-
-    ; @again:
-        ; nop
-        ; jmp @again
-
-        ; lda #%10101100
-        ; jsr lcd_printbin
-        ; lda #' '
-        ; jsr lcd_printchar
-        ; lda #%01010011
-        ; jsr lcd_printbin
-
-        ; Bit-bang all bits of VIA (port B)
-        ; lda #$FF
-        ; sta VIA1_DDRB
-        ; sta VIA1_RB
-    ; @flip:
-        ; inc VIA1_RB
-        ; dec VIA1_RB
-        ; inx
-        ; dex
-        ; jmp @flip
-
-        ; lda #<S_LOADING
-        ; ldx #>S_LOADING
-        ; jsr lcd_printz
-
-    ; @tmp:
-    ;     lda #15
-    ;     sta $D418
-    ;     lda #<S_PROGRESS
-    ;     ldx #>S_PROGRESS
-    ;     jsr lcd_printz
-    ;     lda #0
-    ;     sta $D418
-    ;     lda #<S_PROGRESS
-    ;     ldx #>S_PROGRESS
-    ;     jsr lcd_printz
-    ;     jmp @tmp
 
         ; load song into RAM
         ; ldy #0
